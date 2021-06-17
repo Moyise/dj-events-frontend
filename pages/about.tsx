63277c9705c28f7c5c5a9bbb8c1689d317d4cf5e
Moyise <moyisemr@gmail.com>
2021-06-16 20:17:28 -0400

    pages and routing

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus et numquam
+        cumque dolorum? Consequuntur laudantium suscipit rem quod incidunt officia
+        provident modi quis, id unde aspernatur voluptatibus perferendis architecto vitae!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas reiciendis nulla ab
+        facere, ut nostrum cumque delectus nemo dolorum veritatis quos possimus incidunt
+        deserunt repellat totam obcaecati impedit expedita. Blanditiis!
+      </p>
+    </div>
+  );
+}
